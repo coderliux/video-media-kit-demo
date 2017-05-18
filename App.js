6c3171cef6149e8087b5344f038537a23f36969e
Jie.Liu <ecliujie@gmail.com>
2017-05-18 20:03:01 +0800

    update to rn0.43 add fullscreen auto rotation

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  Text,
-  View,
-  Dimensions,
-  ScrollView,
-  TouchableOpacity
+    Text,
+    View,
+    Dimensions,
+    ScrollView,
+    TouchableOpacity
 } from 'react-native';
 
 const {width, height} = Dimensions.get('window');
@@ -14,68 +14,68 @@ const {width, height} = Dimensions.get('window');
 import {Video} from 'react-native-media-kit';
 
 const HTTP = [
-  'http://localhost:8085/video/broadchurch.mp4'
+    'http://localhost:8085/video/broadchurch.mp4'
 ];
 
 const HLS = [
-  'https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8',
-  'http://cdn3.viblast.com/streams/hls/airshow/playlist.m3u8',
-  'http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8',
-  'http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8',
-  'http://vevoplaylist-live.hls.adaptive.level3.net/vevo/ch3/appleman.m3u8',
-  'http://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8',
-  'http://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8'
+    'https://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/bipbop_4x3_variant.m3u8',
+    'http://cdn3.viblast.com/streams/hls/airshow/playlist.m3u8',
+    'http://sample.vodobox.net/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8',
+    'http://content.jwplatform.com/manifests/vM7nH0Kl.m3u8',
+    'http://vevoplaylist-live.hls.adaptive.level3.net/vevo/ch3/appleman.m3u8',
+    'http://playertest.longtailvideo.com/adaptive/captions/playlist.m3u8',
+    'http://playertest.longtailvideo.com/adaptive/oceans_aes/oceans_aes.m3u8'
 ];
 
 
 export default class App extends Component {
 
-  state = {
-    muted: false,
-    width: width,
-    height: width / (16/9),
-    controls: true
-  };
+    _screenRotation(across) {
+        console.log("update screen:"+across)
+        if (across) {
+            this.setState({
+                width: height,
+                height: width,
+                marginTop:0,
+            })
+        } else {
+            this.setState({
+                width: width,
+                height: width / (16 / 9),
+                marginTop:22
+            })
+        }
+    }
 
-  render() {
-    return (
-      <ScrollView
-        style={{flex: 1, backgroundColor: '#efefef'}}>
-        <Video
-          style={{width: this.state.width, height: this.state.height, marginTop: 50, backgroundColor: 'black'}}
-          autoplay={false}
-          preload='auto'
-          loop={false}
-          controls={this.state.controls}
-          muted={this.state.muted}
-          src={HTTP[0]}
-        />
+    state = {
+        muted: false,
+        width: width,
+        height: width / (16 / 9),
+        controls: true,
+        marginTop:22
+    };
 
-        <View
-          style={{flexDirection: 'row', height: 40}}>
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({
-                muted: !this.state.muted
-              })
-            }}
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>toggle muted</Text>
-          </TouchableOpacity>
+    render() {
+        return (
+            <ScrollView
+                style={{flex: 1, backgroundColor: '#efefef'}}>
+                <Video
+                    style={{
+                        width: this.state.width,
+                        height: this.state.height,
+                        marginTop:this.state.marginTop,
+                        backgroundColor: 'black'
+                    }}
+                    autoplay={false}
+                    preload='auto'
+                    loop={false}
+                    controls={this.state.controls}
+                    muted={this.state.muted}
+                    src={HTTP[0]}
+                    screenRotation={this._screenRotation.bind(this)}
+                />
 
-          <TouchableOpacity
-            onPress={() => {
-              this.setState({
-                width: 160,
-                height: 90,
-                controls: false
-              })
-            }}
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>change layout</Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
-    );
-  }
+            </ScrollView>
+        );
+    }
 }
